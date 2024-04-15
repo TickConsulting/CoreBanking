da6af4e70744b6ce62dd17eee275d27e4f78c25b
ONGIDI E. GEOFREY <ongidigeofrey@gmail.com>
2024-04-15 17:11:12 +0300

    Fix: add database connection

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,7 +1,7 @@
 <?php
-	if(!preg_match('/local/', $_SERVER['HTTP_HOST'])){
+	if(preg_match('/local/', $_SERVER['HTTP_HOST'])){
 		// die('in');
-    	echo file_get_contents('https://websacco.com/home');
+    	echo file_get_contents('https://tickconsulting.co.ke');
 	}else{
 		// echo 'No landing page on local please go to login.';
 		redirect(site_url('login'));
