a4347fa19a53d92f55c80797ee2659891b690d5e
ONGIDI E. GEOFREY <ongidigeofrey@gmail.com>
2024-04-16 20:41:52 +0300

    Fix: edit .htaccess

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,9 +1,10 @@
 <?php
 	if(preg_match('/local/', $_SERVER['HTTP_HOST'])){
-		// die('in');
-    	echo file_get_contents('https://tickconsulting.co.ke');
+		redirect(site_url('login'));
 	}else{
 		// echo 'No landing page on local please go to login.';
-		redirect(site_url('login'));
+    	echo file_get_contents('https://tickconsulting.co.ke');
+
+		
 	}
 ?>
