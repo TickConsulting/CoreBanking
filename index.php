e57d5eda8c10dcdcb747074799d3faff642efd98
ONGIDI E. GEOFREY <ongidigeofrey@gmail.com>
2024-05-03 15:09:35 +0300

    Fix: switch to production mode

--- index.php
+++ index.php
@@ -60,13 +60,13 @@
 		// "105.163.0.64"
 	);
 
-	// if(in_array($_SERVER['REMOTE_ADDR'], $ip_addresses) || preg_match('/local/', $_SERVER['HTTP_HOST'])){
-	// 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	if(in_array($_SERVER['REMOTE_ADDR'], $ip_addresses) || preg_match('/local/', $_SERVER['HTTP_HOST'])){
+		define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 		
-	// }else{
-	// 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');	
-	// }
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	}else{
+		define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');	
+	}
+	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 	define('IPADDESSES',$ip_addresses);
 /*
